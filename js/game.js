620911a21a97753adc715af64d18199496f3b88f
FelipeKreulich <felipek2002k@gmail.com>
2023-03-26 02:19:54 -0400

    Create Controls, Sprite

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
-const canvas = document.querySelector("canvas"); //Request da Tag Canvas
-const ctx = canvas.getContext("2d"); // Criando o Contexto para trabalhar com o 2D
+const canvas = document.querySelector("canvas"); 
+const ctx = canvas.getContext("2d"); 
 
-// Criando a constante para altura e largura
 const canvasWidth = 1024; 
 const canvasHeight = 576;
 
@@ -12,21 +11,21 @@ let prevTime = 0;
 
 animate();
 
-// Criando uma função pra sempre estar animando o canvas conforme o FPS do PC
 function animate() {
   window.requestAnimationFrame(animate);
-  // Alterando o fundo do Game
+
+  handleControls();
+
   ctx.fillStyle = "black";
-  // Usando a propriedade para preencher a tela partindo do ponto 0,0
   ctx.fillRect(0,0, canvasWidth, canvasHeight);
 
-  player.draw(); // Exibindo o player na tela
+  background.update();
+  player.update(); 
+  //player2.update();
 
-  // Exibir o FPS nos logs do console
   let delta = (performance.now() - prevTime) / 1000;
   let fps = 1 / delta;
 
   prevTime = performance.now();
   //console.log(`FPS: ${fps}`);
-
 }
